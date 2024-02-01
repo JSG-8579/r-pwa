b86300034fa7edbb465d8497db7a233a825ad180
JSG-8579 <rkfntmdrhks@naver.com>
2024-02-01 10:59:30 +0900

    자세히보기 닫기 추가

--- public/sw.js
+++ public/sw.js
@@ -12,7 +12,31 @@ self.addEventListener('fetch', (a)=>{
 
 self.addEventListener('message', (event)=>{
     console.log('엣..? 메시지가', event.data)
-    self.registration.showNotification('title',{
+    const option = {
         body: event.data.message,
-    });
+        icon:'', //제목옆에 작은 원형이미지
+        image:'./icon/favicon-196x196.png', //내용 썸네일
+        badge:'',
+        vibrate:[200, 100, 300], //진동울리는 간격
+        actions:[
+            {action:'open', title:'자세히보기'},
+            {action:'close', title:'닫기'},
+        ]
+    }
+
+    self.registration.showNotification('title',option);
 })
+
+
+self.addEventListener('notificationclick', (event)=>{
+    console.log(event.action)
+    console.log(clients)
+    if(event.action == 'open'){
+        //자세히보기
+        clients.openWindow('https://naver.com')
+    }else{
+        //닫기
+        event.notification.close();
+    }
+});
+
