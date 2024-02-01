064f70b35fe2e0cd00b68ad5504124426f79ef3b
JSG-8579 <rkfntmdrhks@naver.com>
2024-02-01 11:25:56 +0900

    clients 오류 해결

--- src/service-worker.js
+++ src/service-worker.js
@@ -69,34 +69,41 @@ registerRoute(
 //   }
 // });
 
-self.addEventListener('message', (event)=>{
+self.addEventListener('message', (event) => {
   console.log('엣..? 메시지가', event.data)
   const option = {
-      body: event.data.message,
-      icon:'', //제목옆에 작은 원형이미지
-      image:'./icon/favicon-196x196.png', //내용 썸네일
-      badge:'',
-      vibrate:[200, 100, 300], //진동울리는 간격
-      actions:[
-          {action:'open', title:'자세히보기'},
-          {action:'close', title:'닫기'},
-      ]
+    body: event.data.message,
+    icon: '', //제목옆에 작은 원형이미지
+    image: './icon/favicon-196x196.png', //내용 썸네일
+    badge: '',
+    vibrate: [200, 100, 300], //진동울리는 간격
+    actions: [
+      { action: 'open', title: '자세히보기' },
+      { action: 'close', title: '닫기' },
+    ]
   }
 
-  self.registration.showNotification('title',option);
+  self.registration.showNotification('title', option);
 })
 
 
-self.addEventListener('notificationclick', (event)=>{
-  // console.log(event.action)
-  // console.log(clients)
-  if(event.action == 'open'){
+self.addEventListener('notificationclick', (event) => {
+  console.log(event.action)
+  console.log(clients)
+  self.clients.matchAll().then(function (clientList) {
+
+    if (event.action == 'open') {
       //자세히보기
-      // clients.openWindow('https://naver.com')
-  }else{
+      event.waitUntil(self.clients.claim())
+
+      clients.openWindow('https://naver.com')
+    } else {
       //닫기
       event.notification.close();
-  }
+    }
+
+  })
+
 });
 
 // Any other custom service worker logic can go here.
